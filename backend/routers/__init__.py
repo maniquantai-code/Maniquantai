33d7f73dc0895941360be48175bdb5abd65932a6
ManiquantAi <maniquantai@gmail.com>
2026-08-22 09:14:55 +0530

    Register explicit paper decision router

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -5,5 +5,6 @@ from .pipeline_mt5 import api_router as pipeline_router
 from .wallet import api_router as wallet_router
 from .broker_accounts import api_router as broker_accounts_router
 from .mt5_bridge import api_router as mt5_bridge_router
+from .paper_decision import api_router as paper_decision_router
 
-__all__ = ["llm_router", "chat_router", "strategies_router", "pipeline_router", "wallet_router", "broker_accounts_router", "mt5_bridge_router"]
+__all__ = ["llm_router", "chat_router", "strategies_router", "pipeline_router", "wallet_router", "broker_accounts_router", "mt5_bridge_router", "paper_decision_router"]
