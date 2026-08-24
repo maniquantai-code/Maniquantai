f57874e0962bc6bf496eeb8a8ef836677c8c1d87
ManiquantAi <maniquantai@gmail.com>
2026-08-24 21:18:20 +0530

    Register strategy compiler router

--- backend/routers/__init__.py
+++ backend/routers/__init__.py
@@ -6,5 +6,6 @@ from .wallet import api_router as wallet_router
 from .broker_accounts import api_router as broker_accounts_router
 from .mt5_bridge import api_router as mt5_bridge_router
 from .paper_decision import api_router as paper_decision_router
+from .strategy_compiler import api_router as strategy_compiler_router
 
-__all__ = ["llm_router", "chat_router", "strategies_router", "pipeline_router", "wallet_router", "broker_accounts_router", "mt5_bridge_router", "paper_decision_router"]
+__all__ = ["llm_router", "chat_router", "strategies_router", "pipeline_router", "wallet_router", "broker_accounts_router", "mt5_bridge_router", "paper_decision_router", "strategy_compiler_router"]
