4bdc465cdc47458b8c96ea483c5da4e37031d4d2
maniquantai-code <maniquantai@gmail.com>
2026-08-28 13:40:34 +0530

    fix: route /api/* to Python backend

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
-  eslint:     { ignoreDuringBuilds: true },
+  eslint: { ignoreDuringBuilds: true },
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/api/:path*",
+          destination: "/api/index.py",
+        },
+      ],
+    };
+  },
 };
 module.exports = nextConfig;
